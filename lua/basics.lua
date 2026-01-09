8479461a7e8578b9055c919967c4e5365ccf0b18
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2026-01-08 20:47:18 -0800

    a

--- lua/basics.lua
+++ lua/basics.lua
@@ -21,16 +21,23 @@ local config = {
 	-- add sum stuff here
 }
 
+function M.setup(opts)
+	if opts then
+		config = vim.tbl_deep_extend("force", config, opts)
+	end
 
-vim.o.tabstop = config.options.tabstop
-vim.o.shiftwidth = config.options.shiftwidth
-vim.o.nu = config.options.nu
-vim.o.relativenumber = config.options.relativenumber
-vim.o.clipboard = config.options.clipboard
-vim.o.inccommand = config.options.inccommand
-vim.o.laststatus = config.options.laststatus
-vim.o.wrap = config.options.wrap
-vim.o.undofile = config.options.undofile
-vim.o.whichwrap = config.options.undofile
+	if config.enabled then
+		vim.o.tabstop = config.options.tabstop
+		vim.o.shiftwidth = config.options.shiftwidth
+		vim.o.nu = config.options.nu
+		vim.o.relativenumber = config.options.relativenumber
+		vim.o.clipboard = config.options.clipboard
+		vim.o.inccommand = config.options.inccommand
+		vim.o.laststatus = config.options.laststatus
+		vim.o.wrap = config.options.wrap
+		vim.o.undofile = config.options.undofile
+		vim.o.whichwrap = config.options.undofile
+	end
+end
 
 return M
